9029c35b5c9213cdad23940f529f58eb787ad684
Richard Soria <richard.soria@epn.edu.ec>
2025-08-06 13:42:36 -0500

    Versión web generada para Render

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,8 +1,9 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String supabaseUrl = "";
-  static const String supabaseAnonKey = '';
+  static const String supabaseUrl = "https://rzksdeckvwjsglmikurl.supabase.co";
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6a3NkZWNrdndqc2dsbWlrdXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NDY4MTgsImV4cCI6MjA3MDAyMjgxOH0.cL5nvwBeAhZylOMJkzyAtGP2L8vGR-Ycu5xu89-RaQ8';
 
   static SupabaseClient get client => Supabase.instance.client;
 
