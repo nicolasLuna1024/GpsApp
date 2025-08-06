37e75282345b0a7ee5fc5d1af0632f1658c088a9
Richard Soria <richard.soria@epn.edu.ec>
2025-08-06 15:13:02 -0500

    Cambio de nombre e íconos

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3361496602"
+    serviceWorkerVersion: "4039930682"
   }
 });
